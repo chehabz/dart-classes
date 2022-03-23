3d958dc76ddf0859151936272262525f801995de
Mohammad Chehab <chehab.mohammad@gmail.com>
2022-03-23 14:58:34 +0400

    Update person.dart

--- person.dart
+++ person.dart
@@ -5,7 +5,7 @@ void main() {
   
   final employee = Employee(name: "Mohammad Shehab", age: 34, height: 76.5, employeeID: "moha4605", salary: 10.5);
   
-  print(employee.describe());
+  print(employee.toString());
 }
 
 class Person {
@@ -21,6 +21,10 @@ class Person {
   
   String describe() => "Hello, I'm $name. I'm $age years old, " 
     "I'm $height meters tall. My skills are $skills";
+  
+ 
+  @override
+  String toString() => "name: $name, age: $age, height: $height";
 }
 
 class Employee extends Person {
@@ -30,5 +34,7 @@ class Employee extends Person {
   
  Employee({String name = "", int age = 0, double height = 0.0, this.employeeID = "", this.salary = 0}):
   super(name: name, age: age, height: height);
-
+  
+  @override
+  String toString() => "${super.toString()}, employeeID: $employeeID, salary: $salary";
 }
