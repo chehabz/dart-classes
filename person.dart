f43e1727759f89e67c5329a323a57ab8cf3b1df5
Mohammad Chehab <chehab.mohammad@gmail.com>
2022-03-23 09:37:10 +0400

    Update person.dart

--- person.dart
+++ person.dart
@@ -1,20 +1,34 @@
 void main() {
-  final person = Person("Mohammad Shehab", 34, 76.5); 
+  
+  final person = Person(name: "Mohammad Shehab", age: 34, height: 76.5); 
   print(person.describe());
+  
+  final employee = Employee(name: "Mohammad Shehab", age: 34, height: 76.5, employeeID: "moha4605", salary: 10.5);
+  
+  print(employee.describe());
 }
 
 class Person {
   
   static const List<String> defaultSkills = ['reading', 'programming'];
   
-  Person(this.name, this.age, this.height, {this.skills = defaultSkills});
+  Person({this.name = "", this.age = 0, this.height = 0, this.skills = defaultSkills});
   
   final String name;
   final int age;
   final double height;
   final List<String>  skills;
   
-  String describe() => "Hello, I'm $name. I'm $age years old, " +
+  String describe() => "Hello, I'm $name. I'm $age years old, " 
     "I'm $height meters tall. My skills are $skills";
 }
 
+class Employee extends Person {
+  
+ final String employeeID;
+ final double salary;
+  
+ Employee({String name = "", int age = 0, double height = 0.0, this.employeeID = "", this.salary = 0}):
+  super(name: name, age: age, height: height);
+
+}
